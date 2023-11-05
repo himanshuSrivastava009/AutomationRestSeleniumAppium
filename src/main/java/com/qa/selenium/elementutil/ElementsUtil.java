d4d0c4e6955619b8ad7cd54f768fac6e6a043eb8
himanshusrivastava009 <himanshu009@outlook.in>
2023-11-06 01:34:05 +0530

    selenium framework ongoing

--- src/main/java/com/qa/selenium/elementutil/ElementsUtil.java
+++ src/main/java/com/qa/selenium/elementutil/ElementsUtil.java
@@ -1,10 +1,14 @@
 package com.qa.selenium.elementutil;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class ElementsUtil {
@@ -29,5 +33,53 @@ public class ElementsUtil {
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public WebElement getWebElement(By locator) {
+
+        return driver.findElement(locator);
+    }
+
+    public void moveToElementAndPerformAction(By locator) {
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(getWebElement(locator)).build().perform();
+    }
+
+    public void sendKeysUsingAction(By locator, String value) {
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(getWebElement(locator)).sendKeys(value).build().perform();
+    }
+
+    public void holdAndReleaseUsingAction(By source, By destination) {
+        Actions actions = new Actions(driver);
+        actions.clickAndHold().moveToElement(getWebElement(source)).release(getWebElement(destination)).build().perform();
+
+    }
+
+    public void selectByVisibleText(By locator, String visibleText) {
+
+        Select select = new Select(getWebElement(locator));
+        select.selectByVisibleText(visibleText);
+    }
+
+    public void selectByID(By locator, int index) {
+
+        Select select = new Select(getWebElement(locator));
+        select.selectByIndex(index);
+    }
+
+    public void selectByValue(By locator, String value) {
+
+        Select select = new Select(getWebElement(locator));
+        select.selectByValue(value);
+    }
+
+    public void takeScreenShot() throws IOException {
+
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(file, new File("/Users/swati/Documents/HimashuRestAssured/AutomationRestSelenium/ScreenShot" + System.currentTimeMillis() + ".png"));
+
     }
 }
